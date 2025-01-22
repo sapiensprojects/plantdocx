148fdd953c3d2a97c1a292712fe3ba3dd0fde46f
Sapien <onweb.sapien@gmail.com>
2025-01-22 20:22:48 +0530

    adding space around paragraph

--- scripts/plantDocxTypeA/paragraphStyles.js
+++ scripts/plantDocxTypeA/paragraphStyles.js
@@ -15,7 +15,7 @@ export default [
         },
         paragraph: {
             // alignment: AlignmentType.JUSTIFIED,
-            // spacing: { line: 276, before: 20 * 72 * 0.1, after: 20 * 72 * 0.05 },
+            spacing: { line: 276, before: 20 * 72 * 0.1, after: 20 * 72 * 0.05 },
         },
     },
 
@@ -31,7 +31,7 @@ export default [
             bold: true,
         },
         paragraph: {
-            // spacing: { line: 276, before: 20 * 72 * 0.1, after: 20 * 72 * 0.05 },
+            spacing: { line: 276, before: 20 * 72 * 0.1, after: 20 * 72 * 0.05 },
             // rightTabStop: TabStopPosition.MAX,
             // leftTabStop: 453.543307087,
         },
@@ -49,7 +49,7 @@ export default [
             italics: true,
         },
         paragraph: {
-            // spacing: { line: 276, before: 20 * 72 * 0.1, after: 20 * 72 * 0.05 },
+            spacing: { line: 276, before: 20 * 72 * 0.1, after: 20 * 72 * 0.05 },
             // rightTabStop: TabStopPosition.MAX,
             // leftTabStop: 453.543307087,
         },
@@ -68,7 +68,7 @@ export default [
             bold: true
         },
         paragraph: {
-            // spacing: { line: 276, before: 20 * 72 * 0.1, after: 20 * 72 * 0.05 },
+            spacing: { line: 276, before: 20 * 72 * 0.1, after: 20 * 72 * 0.05 },
             // rightTabStop: TabStopPosition.MAX,
             // leftTabStop: 453.543307087,
         },
@@ -89,7 +89,7 @@ export default [
         },
         paragraph: {
             // alignment: AlignmentType.JUSTIFIED,
-            // spacing: { line: 276, before: 20 * 72 * 0.1, after: 20 * 72 * 0.05 },
+            spacing: { line: 276, before: 20 * 72 * 0.1, after: 20 * 72 * 0.05 },
         },
     },
 
@@ -106,7 +106,7 @@ export default [
             color: primaryColorHexValue,
         },
         paragraph: {
-            // spacing: { line: 276, before: 20 * 72 * 0.1, after: 20 * 72 * 0.05 },
+            spacing: { line: 276, before: 20 * 72 * 0.1, after: 20 * 72 * 0.05 },
             // rightTabStop: TabStopPosition.MAX,
             // leftTabStop: 453.543307087,
         },
@@ -125,7 +125,7 @@ export default [
             color: primaryColorHexValue,
         },
         paragraph: {
-            // spacing: { line: 276, before: 20 * 72 * 0.1, after: 20 * 72 * 0.05 },
+            spacing: { line: 276, before: 20 * 72 * 0.1, after: 20 * 72 * 0.05 },
             // rightTabStop: TabStopPosition.MAX,
             // leftTabStop: 453.543307087,
         },
@@ -145,7 +145,7 @@ export default [
             color: primaryColorHexValue,
         },
         paragraph: {
-            // spacing: { line: 276, before: 20 * 72 * 0.1, after: 20 * 72 * 0.05 },
+            spacing: { line: 276, before: 20 * 72 * 0.1, after: 20 * 72 * 0.05 },
             // rightTabStop: TabStopPosition.MAX,
             // leftTabStop: 453.543307087,
         },
